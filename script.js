b20ed65d5d7c89f84e1fe754cdf0e7c360e5973b
Manish Parajuli <parajulimanish43@gmail.com>
2022-03-29 19:28:31 +0545

    newswebsite_ui

---
+++ script.js
@@ -0,0 +1,11 @@
+function hamburger(){
+    document.getElementById("bottomsection").style.display = "block";
+    document.getElementById("hamburger").style.display = "none";
+
+}
+
+function closeHamburger(){
+    document.getElementById("bottomsection").style.display = "none";
+    document.getElementById("hamburger").style.display = "block";
+
+}
